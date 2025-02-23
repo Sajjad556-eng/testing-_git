77e3cd9030e37fa783ca5e40583886278cda4ee4
Sajjad566-eng <itssajjad556@gmail.com>
2025-02-23 20:34:07 +0600

    prifix and postfix oparators

--- OneDrive/Desktop/JS.bignner/oparation.js
+++ OneDrive/Desktop/JS.bignner/oparation.js
@@ -22,11 +22,32 @@ console.log(str3);*/
 
 //complex situation
 
-console.log("1" +2);
+/*console.log("1" +2);
 console.log(1+"2");
 console.log("1"+"2");
 console.log("1"+2+2);
-console.log(+2+"2");
+console.log(+2+"2");*/
+
+//special convertaion
+
+/*console.log(true);
+console.log(+true);
+console.log(true+);
+console.log(+"");*/
+
+let num1,num2,num3
+num1=num2=num3=2+2
+//console.log(num1);
+
+let gameCounter = 100
+//++gameCounter
+gameCounter++ // prifix and postfix oparators ++
+console.log(gameCounter);
+
+
+
+
+
 
 
 
