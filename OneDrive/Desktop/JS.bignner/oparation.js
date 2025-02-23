5914466149f5b31b4684e0d8b73d64f7eca245e6
Sajjad566-eng <itssajjad556@gmail.com>
2025-02-23 20:08:57 +0600

    opartion hiow it works

---
+++ OneDrive/Desktop/JS.bignner/oparation.js
@@ -0,0 +1,37 @@
+/* oparation 
+
+oparation hoilo kono kisur add+ multiply* agulai hoilo oparation*/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(3%2);*/
+
+//TRicky things
+
+/*let str1 = "Hi"
+let str2 = "Sajjad"
+let str3 = str1+str2
+console.log(str3);*/
+
+//complex situation
+
+console.log("1" +2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(+2+"2");
+
+
+
+
+
+
+
+
