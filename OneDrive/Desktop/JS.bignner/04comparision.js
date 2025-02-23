1743b1f483ce5078cd9525e0841b8bd6b3a70f6e
Sajjad566-eng <itssajjad556@gmail.com>
2025-02-23 21:10:32 +0600

    all comparion with strick cheak

---
+++ OneDrive/Desktop/JS.bignner/04comparision.js
@@ -0,0 +1,28 @@
+// comparison // in comparison the output give in boolean
+console.log(2>2);
+/*console.log(2>=1);
+console.log(2<1);
+console.log(2==1)
+console.log(2!=1);
+// data type ak na hoile pridicted output ase na 
+// comarison korar somoi kheyal rakte ho be je datatype jate same thakw 
+
+console.log("2">1);
+console.log("02>"1);
+
+//JS a comparison and equility cheack dui ta alada jinis
+console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);S
+ console.log(undefined>0);
+ console.log(undefined==0);
+ console.log(undefined>=0);
+ 
+ //strick cheak "===" aytai ar ay khane duita variable ar datatype same hoite hoi 
+
+ console.log(2===2);
+ console.log("2"===2);
+
+ */
+ 
+
