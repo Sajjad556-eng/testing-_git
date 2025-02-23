bf9093f90e27b36de6926b52bfa8437a7edf620d
Sajjad566-eng <itssajjad556@gmail.com>
2025-02-23 21:44:20 +0600

    all what i have learn

---
+++ OneDrive/Desktop/JS.bignner/dattype_summary.js
@@ -0,0 +1,4 @@
+// there are 2 types of datatype that store and access from the memory  1.primitive datatype 2.nonprimitive datatype
+// there are 7 types of datatype in primitive datatype 1.number 2.string 3.boolearn 4.null 5.undifined 6.symbol 7.Bigint
+// in nonprimitive ot referance type threre are 3 types of datatype 1array 2.function 3.objects
+ 
