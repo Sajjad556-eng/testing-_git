55da182e3e961e7b0fd06291d3a5b318feb8aef7
Sajjad566-eng <itssajjad556@gmail.com>
2025-02-24 00:01:56 +0600

    summary of datatype fullfill

--- OneDrive/Desktop/JS.bignner/dattype_summary.js
+++ OneDrive/Desktop/JS.bignner/dattype_summary.js
@@ -1,4 +1,40 @@
 // there are 2 types of datatype that store and access from the memory  1.primitive datatype 2.nonprimitive datatype
-// there are 7 types of datatype in primitive datatype 1.number 2.string 3.boolearn 4.null 5.undifined 6.symbol 7.Bigint
-// in nonprimitive ot referance type threre are 3 types of datatype 1array 2.function 3.objects
- 
+// there are 7 types of datatype in primitive datatype 1.number 2.string 3.boolean 4.null 5.undefined 6.symbol 7.Bigint
+// in nonprimitive ot reference type there are 3 types of datatype 1array 2.function 3.objects
+/*let score = 55
+let name = sajjad
+let temp = null
+let value
+console.log(typeof score,name,temp,value);*/
+/*const score = 100
+const scoreValue = 100.3
+console.log( scoreValue)
+
+*/
+
+//Symbol -- in symbol every single things are uniq 
+
+/*const id = Symbol('123')
+const anotherID = Symbol('123')
+console.log(id===anotherID);*/
+
+/*const bigNumber = 4451225555445122155n
+console.log(typeof bigNumber);//output bigint
+*/
+
+// nonprimitive array / array sob somoi [] a bose
+
+/*const hero = ["Ironman", "Hulk", "Thor"]
+console.log(hero);
+*/
+
+//object----- object sob somoi {} ar vitore bose 
+
+/*    name = "sajjad",
+    age = 22
+}
+console.log(user);
+*/
+
+// function -- sobsomoi () {} ar modhey bose
+
